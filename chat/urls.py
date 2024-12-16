d4813337cb6fd805761408e544c7f3b3cdab316e
Harshith-Pyata <harshithpyata2808@gmail.com>
2024-12-16 17:52:48 +0530

    final draft i think so

--- chat/urls.py
+++ chat/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('<str:Room>/', views.Room, name='Room'),
     path('checkview',views.checkview, name='checkview'),
     path('send',views.send, name='send'),
+    path('getroom/<str:Room>/', views.getroom, name='getroom'),
     path('getMessages/<str:Room>/', views.getMessages, name='getMessages')
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import room, message
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 def home(request):
@@ -15,17 +17,22 @@ def Room(request,Room):
         'room_details': room_details
     })
 
+@csrf_exempt
 def checkview(request):
     Room_name = request.POST['room_name']
     username = request.POST['username']
 
     if room.objects.filter(name=Room_name).exists():
         return redirect('/'+Room_name+'?username='+username)
+        # return JsonResponse({'message':"room exits"})
     else:
         new_room = room.objects.create(name = Room_name)
         new_room.save()
         return redirect('/'+Room_name+'?username='+username)
+        # return JsonResponse({'message':"room created"})
+    # return JsonResponse({'status' : "fail", "message": "Invalid request method"})
 
+@csrf_exempt
 def send(request):
     Message = request.POST['message']
     Username = request.POST['username']
@@ -36,8 +43,14 @@ def send(request):
 
     return HttpResponse('message sent successfully')
 
+@csrf_exempt
 def getMessages(request,Room):
     room_details = room.objects.get(name = Room)
 
     messages = message.objects.filter(room = room_details.id)
-    return JsonResponse({"messages":list(messages.values()) })
+    return JsonResponse({"messages":list(messages.values())})
+
+@csrf_exempt
+def getroom(request, Room):
+    room_details = room.objects.get(name = Room)
+    return JsonResponse({"room_id": room_details.id})
