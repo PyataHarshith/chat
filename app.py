c72cabb445f92ca97de07bd6a590b7ae519c445d
Harshith-Pyata <harshithpyata2808@gmail.com>
2024-12-16 12:25:22 +0530

    stream app5

--- app.py
+++ app.py
@@ -1,28 +1,3 @@
-# import streamlit as st
-# st.title("Hello, Streamlit!")
-
-# #!/usr/bin/env python
-# """Django's command-line utility for administrative tasks."""
-# import os
-# import sys
-
-
-# def main():
-#     """Run administrative tasks."""
-#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
-#     try:
-#         from django.core.management import execute_from_command_line
-#     except ImportError as exc:
-#         raise ImportError(
-#             "Couldn't import Django. Are you sure it's installed and "
-#             "available on your PYTHONPATH environment variable? Did you "
-#             "forget to activate a virtual environment?"
-#         ) from exc
-#     execute_from_command_line(sys.argv)
-
-
-# if __name__ == '__main__':
-#     main()
 
 import streamlit as st
 import os
@@ -55,7 +30,7 @@ if st.session_state.current_page == "Home":
     st.title("Home Page")
     
     # Load and display the home.html file
-    html_file_path_home = os.path.join(os.getcwd(), 'home.html')
+    html_file_path_home = os.path.join(os.getcwd(), 'templates/home.html')
     html_content_home = load_html_file(html_file_path_home)
     
     st.components.v1.html(html_content_home, height=500)
@@ -64,7 +39,7 @@ elif st.session_state.current_page == "Room":
     st.title("Room Page")
     
     # Load and display the room.html file
-    html_file_path_room = os.path.join(os.getcwd(), 'room.html')
+    html_file_path_room = os.path.join(os.getcwd(), 'templates/room.html')
     html_content_room = load_html_file(html_file_path_room)
     
     st.components.v1.html(html_content_room, height=500)
