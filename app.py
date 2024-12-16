4dfa29a8186b33514882c1314fcff702071274c9
Harshith-Pyata <harshithpyata2808@gmail.com>
2024-12-16 14:37:48 +0530

    streamlit1

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-
 import streamlit as st
 import os
 import requests
 
 
-# harsh_endpoint = "http://127.0.0.1:8000/"
-st.get("http://127.0.0.1:8000/")
+# # harsh_endpoint = "http://127.0.0.1:8000/"
+# requests.get("http://127.0.0.1:8000/")
 
+st.title("Chat Box")
 
+Room_Name = st.text_input("Room Name")
+Username = st.text_input("Username")
+
+if st.button("Enter Room"):
+    data = {
+        "name" : Room_Name
+    }
+    response = requests.post("http://127.0.0.1:8000/checkview")
+
+    if response.status_code == 200:
+        st.success("Succesfully Entered room")
+    else:
+        st.error("Failed to enter room")
